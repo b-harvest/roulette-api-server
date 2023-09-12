5c3e5226ac99101433cbbe5637adb1a8ad4c0fac
KyuMin Jeong <qmin80@gmail.com>
2023-09-12 17:23:29 +0900

    Update promotion.go

--- models/schema/promotion.go
+++ models/schema/promotion.go
@@ -8,6 +8,7 @@ type PromotionRow struct {
 	PromotionId            int64     `json:"promotionId" db:"promotion_id"`
 	Title 			           string    `json:"title" db:"title"`
 	Desc 			             string    `json:"desc" db:"desc"`
+	Url                   string    `json:"url" db:"url"`
 	IsActive               bool      `json:"isActive" db:"is_active"`
 	IsWhitelisted          bool      `json:"isWhitelisted" db:"is_whitelisted"`
 	VoucherName 			     string    `json:"voucherName" db:"voucher_name"`
