8f0f4a4e637801188bf2a57a91a8487e56310acb
KyuMin Jeong <qmin80@gmail.com>
2023-09-12 17:23:06 +0900

    Update httpRequest.go

--- types/httpRequest.go
+++ types/httpRequest.go
@@ -23,6 +23,7 @@ type ReqTbUpdateGame struct {
 type ReqTbCreatePromotion struct {
 	Title 			           string    `json:"title" db:"title"`
 	Desc 			             string    `json:"desc" db:"desc"`
+	Url                   string    `json:"url" db:"url"`
 	IsActive               bool      `json:"isActive" db:"is_active"`
 	IsWhitelisted          bool      `json:"isWhitelisted" db:"is_whitelisted"`
 	VoucherName 			     string    `json:"voucherName" db:"voucher_name"`
