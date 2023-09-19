0eb17754d575b739558334350ead52d39d7de3e0
parkjihon <jihonee1@gmail.com>
2023-09-19 19:40:10 +0900

    fix: add getLatestOrder

--- controllers/account.go
+++ controllers/account.go
@@ -10,6 +10,7 @@ import (
 	"roulette-api-server/models/schema"
 	"roulette-api-server/services"
 	"roulette-api-server/types"
+	"strconv"
 	"strings"
 	"time"
 
@@ -70,6 +71,34 @@ func GetGameOrdersByAddr(c *gin.Context) {
 	}
 
 	services.Success(c, nil, &orders)
+}
+
+// 유저 별 최근 주문 (game-id 필요)
+func GetLatestOrder(c *gin.Context) {
+	addr := c.Param("addr")
+	strGameId := c.Query("game-id")
+	if strGameId == "" {
+		services.NotAcceptable(c, "invalid gameId "+c.Request.Method+" "+c.Request.RequestURI, nil)
+		return
+	}
+	gameId, err := strconv.ParseInt(strGameId, 10, 64)
+	if err != nil {
+		services.NotAcceptable(c, "invalid gameId "+c.Request.Method+" "+c.Request.RequestURI+" : "+err.Error(), err)
+		return
+	}
+	order := types.ResGetLatestOrderByAddr{
+		Addr: addr,
+		GameId: gameId,
+	}
+
+	err = models.QueryLatestOrderByAddr(&order)
+	if err != nil {
+		fmt.Printf("%+v\n", err.Error())
+		services.NotAcceptable(c, "fail "+c.Request.Method+" "+c.Request.RequestURI+" : "+err.Error(), err)
+		return
+	}
+
+	services.Success(c, nil, &order)
 }
 
 func GetWinTotalByAcc(c *gin.Context) {
